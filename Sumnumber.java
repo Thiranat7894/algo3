50128538a50f7a83431f4936a9479e1d1e35ddec
Thiranat7894 <138641542+Thiranat7894@users.noreply.github.com>
2024-07-30 10:56:08 +0700

    Thiranat

---
+++ Sumnumber.java
@@ -0,0 +1,12 @@
+public class Sumnumber
+    {
+        public static void main (String args[])
+        {
+            int num1 = 4;
+            int num2 = 6;
+            int sum = num1 + num2;
+            System.out.println(sum);
+
+            
+        }
+    }
---
+++ W1/DisplayText.java
@@ -0,0 +1,8 @@
+public class DisplayText {
+    public static void main(String[] args) {
+        System.out.println("Nattaphon" + " Klinjun" );
+        System.out.println(10+8);
+        System.out.println("NPRU" + 2567);
+        System.out.println(0+ "3" + 2);
+    }
+}
